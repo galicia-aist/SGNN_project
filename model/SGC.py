c9bdaec8ce3f3a84e815aa0677ebaf436f2e286e
Gustavo Galicia <gustavo.galicia@aist.go.jp>
2025-04-02 15:11:08 +0900

    reset settings for SGC and config

--- model/SGC.py
+++ model/SGC.py
@@ -9,14 +9,10 @@ from torch.nn import Linear
 class SGC(torch.nn.Module):
     def __init__(self, data):
         super(SGC, self).__init__()
-        hidden_dim = 64  # Specify the hidden dimension for the intermediate layer
-        self.conv1 = SGConv(data.num_features, hidden_dim, K=2)  # First layer
-        self.conv2 = SGConv(hidden_dim, data.num_classes, K=2)  # Second layer
+        self.conv = SGConv(data.num_features, data.num_classes, K=3)  # K=2 means using a 2-hop neighborhood
 
     def forward(self, data):
-        x = self.conv1(data.x, data.edge_index)
-        x = F.relu(x)  # Add non-linearity after the first layer
-        x = self.conv2(x, data.edge_index)
+        x = self.conv(data.x, data.edge_index)
         return F.log_softmax(x, dim=1)
 
 
