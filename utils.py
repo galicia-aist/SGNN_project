31566cc8bad86502f3c7e251b50eeaf254804e48
Gustavo Galicia <gustavo.galicia@aist.go.jp>
2025-04-18 15:07:15 +0900

    fix log

--- utils.py
+++ utils.py
@@ -323,7 +323,7 @@ def get_logger():
         logs_dir = os.path.join(os.getcwd(), "logs")
         if not os.path.exists(logs_dir):
             os.makedirs(logs_dir)
-        log_path = f"{logs_dir}//{model}_{dataset_name}.log"
+        log_path = f"{logs_dir}//{model}Concat_{dataset_name}.log"
 
 
 
